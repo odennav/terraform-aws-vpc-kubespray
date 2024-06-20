714926e3cc5ec6f186d31f6ffbae797671d97b5e
Victor Odeghe <Odeghe Nnanna Victor>
2024-06-20 10:25:29 +0100

    terraform-manifest dir renamed to terraform

---
+++ terraform/1a-versions.tf
@@ -0,0 +1,16 @@
+# Terraform Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
+
+}
